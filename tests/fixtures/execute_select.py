4d4fdec088b32ce0b8ce1d5d83b909128e08cf20
unwnu <ruslansolominskii@gmail.com>
2020-04-10 21:45:37 +0300

    fixtures/execute_select add::execute_select_notice_compiled_release

--- tests/fixtures/execute_select.py
+++ tests/fixtures/execute_select.py
@@ -7,3 +7,11 @@ def execute_select_revision_amendments_by_id(prepared_select_revision_amendments
         return cassandra_session.execute(prepared_select_revision_amendments_by_id, [cpid, ocid, id])
 
     return with_values
+
+
+@pytest.fixture(scope='function')
+def execute_select_notice_compiled_release(prepared_select_notice_compiled_release, cassandra_session):
+    def with_values(cpid, ocid):
+        return cassandra_session.execute(prepared_select_notice_compiled_release, [cpid, ocid])
+
+    return with_values
