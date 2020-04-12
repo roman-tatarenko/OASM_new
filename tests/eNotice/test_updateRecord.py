70559aabf2cc2541c8280f1b4a3040eecee4080b
unwnu <ruslansolominskii@gmail.com>
2020-04-12 11:48:55 +0300

    eNotice/test_updateRecord add::C8260,C8261,C8262

--- tests/eNotice/test_updateRecord.py
+++ tests/eNotice/test_updateRecord.py
@@ -11,7 +11,7 @@ from pytest_testrail.plugin import pytestrail
 def test_on_possibility_to_update_record_with_a_valid_data_all_attributes_if_amendment_from_request_relates_to_tender(
         host, port, execute_insert_into_notice_compiled_release, prepared_cpid, prepared_ev_ocid, prepared_token_entity,
         prepared_release_id, payload_notice_compiled_release, execute_select_notice_compiled_release,
-        prepared_data_add_EV_before_SendAcForVerification, response_success):
+        prepared_data_add_EV_before_SendAcForVerification, response):
     data = prepared_data_add_EV_before_SendAcForVerification
 
     execute_insert_into_notice_compiled_release(cp_id=prepared_cpid, oc_id=prepared_ev_ocid, json_data=data,
@@ -27,7 +27,7 @@ def test_on_possibility_to_update_record_with_a_valid_data_all_attributes_if_ame
     del amendment_request['token']
 
     check.equal(amendment_request, amendment_db)
-    check.equal(actualresult, response_success)
+    check.equal(actualresult, response.success)
 
 
 @pytestrail.case('C8375')
@@ -35,7 +35,7 @@ def test_on_possibility_to_update_record_with_a_valid_data_all_attributes_if_ame
         host, port, execute_insert_into_notice_compiled_release, prepared_cpid, prepared_ev_ocid, prepared_token_entity,
         prepared_release_id, payload_notice_compiled_release, execute_select_notice_compiled_release,
         data_for_test_notice_compiled_release,
-        prepared_data_add_EV_before_SendAcForVerification, response_success):
+        prepared_data_add_EV_before_SendAcForVerification, response):
     data = prepared_data_add_EV_before_SendAcForVerification
 
     execute_insert_into_notice_compiled_release(cp_id=prepared_cpid, oc_id=prepared_ev_ocid, json_data=data,
@@ -54,7 +54,7 @@ def test_on_possibility_to_update_record_with_a_valid_data_all_attributes_if_ame
     del amendment_request['token']
 
     check.equal(amendment_request, amendment_db)
-    check.equal(actualresult, response_success)
+    check.equal(actualresult, response.success)
 
 
 @pytest.mark.parametrize("param",
@@ -70,7 +70,7 @@ def test_on_a_possibility_to_update_record_without_params_tender_amendments_desc
         host, port, execute_insert_into_notice_compiled_release, prepared_cpid, prepared_ev_ocid, prepared_token_entity,
         prepared_release_id, payload_notice_compiled_release, execute_select_notice_compiled_release,
         data_for_test_notice_compiled_release, param,
-        prepared_data_add_EV_before_SendAcForVerification, response_success):
+        prepared_data_add_EV_before_SendAcForVerification, response):
     data = prepared_data_add_EV_before_SendAcForVerification
 
     execute_insert_into_notice_compiled_release(cp_id=prepared_cpid, oc_id=prepared_ev_ocid, json_data=data,
@@ -89,7 +89,7 @@ def test_on_a_possibility_to_update_record_without_params_tender_amendments_desc
     del amendment_request['token']
 
     check.equal(amendment_request, amendment_db)
-    check.equal(actualresult, response_success)
+    check.equal(actualresult, response.success)
 
 
 @pytestrail.case('C8258')
@@ -97,7 +97,7 @@ def test_on_possibility_to_update_record_without_params_tender_amendments_docume
         host, port, execute_insert_into_notice_compiled_release, prepared_cpid, prepared_ev_ocid, prepared_token_entity,
         prepared_release_id, payload_notice_compiled_release, execute_select_notice_compiled_release,
         data_for_test_notice_compiled_release,
-        prepared_data_add_EV_before_SendAcForVerification, response_success):
+        prepared_data_add_EV_before_SendAcForVerification, response):
     data = prepared_data_add_EV_before_SendAcForVerification
 
     execute_insert_into_notice_compiled_release(cp_id=prepared_cpid, oc_id=prepared_ev_ocid, json_data=data,
@@ -116,7 +116,7 @@ def test_on_possibility_to_update_record_without_params_tender_amendments_docume
     del amendment_request['token']
 
     check.equal(amendment_request, amendment_db)
-    check.equal(actualresult, response_success)
+    check.equal(actualresult, response.success)
 
 
 @pytestrail.case('C8314')
@@ -124,7 +124,7 @@ def test_eNotice_adds_amendment_object_after_release_updating_if_release_already
         host, port, execute_insert_into_notice_compiled_release, prepared_cpid, prepared_ev_ocid, prepared_token_entity,
         prepared_release_id, payload_notice_compiled_release, execute_select_notice_compiled_release,
         data_for_test_notice_compiled_release,
-        data_ev_with_amendment, response_success):
+        data_ev_with_amendment, response):
     data = data_ev_with_amendment
 
     execute_insert_into_notice_compiled_release(cp_id=prepared_cpid, oc_id=prepared_ev_ocid, json_data=data,
@@ -138,6 +138,29 @@ def test_eNotice_adds_amendment_object_after_release_updating_if_release_already
     amendment_request = json.loads(payload['params']['data'])['tender']['amendments'][0]
     del amendment_request['token']
     amendment_data = data['tender']['amendments'][0]
-    check.equal(actualresult, response_success)
+    check.equal(actualresult, response.success)
 
     assert all(item in amendment_db for item in [amendment_request, amendment_data])
+
+
+@pytest.mark.parametrize("param",
+                         [
+                             pytest.param("tender", marks=pytestrail.case('C8260')),
+                             pytest.param("ocid", marks=pytestrail.case('C8261')),
+                             pytest.param("cpid", marks=pytestrail.case('C8262'))
+                         ])
+def test_on_there_is_response_with_status_error_if_request_does_not_contain(
+        host, port, param, payload_notice_compiled_release,
+        data_for_test_notice_compiled_release, response):
+    del data_for_test_notice_compiled_release[param]
+
+    payload = payload_notice_compiled_release(data=data_for_test_notice_compiled_release)
+    actualresult = requests.post(f'{host}:{port.eNotice}/command2', json=payload).json()
+    response.error['result'] = [
+        {
+            "code": "RQ-1/2",
+            "description": "Can not parse 'data'."
+        }
+    ]
+
+    assert actualresult == response.error
