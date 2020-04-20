f4194d70dadd1074438c4b2d455ab39253de37cc
unwnu <ruslansolominskii@gmail.com>
2020-04-20 15:04:00 +0300

    eAccess refactor::fixtures,response

--- tests/eAccess/test_checkPersonesStructure.py
+++ tests/eAccess/test_checkPersonesStructure.py
@@ -6,7 +6,7 @@ from pytest_testrail.plugin import pytestrail
 @pytestrail.case('C13271')
 def test_checkPersonesStructure_if_a_request_contains_valid_data(host, port, payload_checkPersonesStructure,
                                                                  response, data_person):
-    payload = payload_checkPersonesStructure(persones=[data_person], locationOfPersones='award')
+    payload = payload_checkPersonesStructure(data_person, locationOfPersones='award')
     actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
 
     assert actualresult == response.success
@@ -15,8 +15,9 @@ def test_checkPersonesStructure_if_a_request_contains_valid_data(host, port, pay
 @pytestrail.case('C13272')
 def test_checkPersonesStructure_if_a_request_does_not_contain_identifier_uri(host, port, payload_checkPersonesStructure,
                                                                              response, data_person):
-    del data_person['identifier']['uri']
-    payload = payload_checkPersonesStructure(persones=[data_person], locationOfPersones='award')
+    person = data_person
+    del person['identifier']['uri']
+    payload = payload_checkPersonesStructure(person, locationOfPersones='award')
     actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
 
     assert actualresult == response.success
@@ -27,9 +28,9 @@ def test_checkPersonesStructure_if_a_request_does_not_contain_businessFunctions_
                                                                                           data_person,
                                                                                           payload_checkPersonesStructure
                                                                                           ):
-    person_1 = data_person
-    del data_person['businessFunctions'][0]['documents']
-    payload = payload_checkPersonesStructure(persones=[person_1], locationOfPersones='award')
+    person = data_person
+    del person['businessFunctions'][0]['documents']
+    payload = payload_checkPersonesStructure(person, locationOfPersones='award')
     actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
 
     assert actualresult == response.success
@@ -40,9 +41,9 @@ def test_checkPersonesStructure_if_a_request_does_not_contain_documents_descript
                                                                                     data_person,
                                                                                     payload_checkPersonesStructure
                                                                                     ):
-    person_1 = data_person
-    del data_person['businessFunctions'][0]['documents'][0]['description']
-    payload = payload_checkPersonesStructure(persones=[person_1], locationOfPersones='award')
+    person = data_person
+    del person['businessFunctions'][0]['documents'][0]['description']
+    payload = payload_checkPersonesStructure(person, locationOfPersones='award')
     actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
 
     assert actualresult == response.success
@@ -55,7 +56,7 @@ def test_checkPersonesStructure_if_a_request_contains_more_than_one_persones_obj
                                                                                     ):
     person_1 = data_person
     person_2 = data_person
-    payload = payload_checkPersonesStructure(persones=[person_1, person_2], locationOfPersones='award')
+    payload = payload_checkPersonesStructure(person_1, person_2, locationOfPersones='award')
     actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
 
     assert actualresult == response.success
@@ -77,19 +78,14 @@ def test_checkPersonesStructure_if_a_request_contains_more_than_one_persones_obj
 def test_checkPersonesStructure_if_data_of_attribute_mismatch_to_the_pattern(host, port, data_person, response,
                                                                              param, value, code, description,
                                                                              payload_checkPersonesStructure):
-    person = data_person
-    payload = payload_checkPersonesStructure(persones=[person], locationOfPersones='award')
+    payload = payload_checkPersonesStructure(locationOfPersones='award')
     payload['params'][param] = value
     actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
     response.error['result'] = [
         {
             "code": code,
             "description": description,
-            "details": [
-                {
-                    "name": param
-                }
-            ]
+            "details": [{"name": param}]
         }
     ]
 
@@ -103,7 +99,7 @@ def test_checkPersonesStructure_a_request_contains_more_than_one_businessFunctio
                                                                                           payload_checkPersonesStructure):
     person = data_person
     person['businessFunctions'].append(data_businessFunction)
-    payload = payload_checkPersonesStructure(persones=[person], locationOfPersones='award')
+    payload = payload_checkPersonesStructure(person, locationOfPersones='award')
     actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
 
     assert actualresult == response.success
@@ -115,7 +111,7 @@ def test_checkPersonesStructure_a_request_contains_invalid_enum_for_businessFunc
                                                                                            response):
     person = data_person
     person['businessFunctions'][0]['type'] = 'authority'
-    payload = payload_checkPersonesStructure(persones=[person], locationOfPersones='award')
+    payload = payload_checkPersonesStructure(person, locationOfPersones='award')
     actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
 
     response.error['result'] = [
@@ -125,11 +121,7 @@ def test_checkPersonesStructure_a_request_contains_invalid_enum_for_businessFunc
                            " Expected values: 'chairman, procurementOfficer, contactPoint,"
                            " technicalEvaluator, technicalOpener, priceOpener, priceEvaluator',"
                            " actual value: 'authority'.",
-            "details": [
-                {
-                    "name": "businessFunction.type"
-                }
-            ]
+            "details": [{"name": "businessFunction.type"}]
         }
     ]
 
@@ -142,7 +134,7 @@ def test_checkPersonesStructure_a_request_contains_invalid_enum_for_documentType
                                                                                  response):
     person = data_person
     person['businessFunctions'][0]['documents'][0]['documentType'] = 'x_eligibilityDocuments'
-    payload = payload_checkPersonesStructure(persones=[person], locationOfPersones='award')
+    payload = payload_checkPersonesStructure(person, locationOfPersones='award')
     actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
 
     response.error['result'] = [
@@ -150,11 +142,7 @@ def test_checkPersonesStructure_a_request_contains_invalid_enum_for_documentType
             "code": "DR-3/3",
             "description": "Attribute value mismatch with one of enum expected values."
                            " Expected values: 'regulatoryDocument', actual value: 'x_eligibilityDocuments'.",
-            "details": [
-                {
-                    "name": "documentType"
-                }
-            ]
+            "details": [{"name": "documentType"}]
         }
     ]
 
@@ -168,7 +156,7 @@ def test_checkPersonesStructure_a_request_contains_more_than_one_document_object
                                                                                  response):
     person = data_person
     person['businessFunctions'][0]['documents'].append(data_document)
-    payload = payload_checkPersonesStructure(persones=[person], locationOfPersones='award')
+    payload = payload_checkPersonesStructure(person, locationOfPersones='award')
     actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
 
     assert actualresult == response.success
--- tests/eAccess/test_request_body.py
+++ tests/eAccess/test_request_body.py
@@ -85,3 +85,174 @@ def test_findLotIds_request_does_not_contain_params_object(port, host, response,
     ]
 
     assert actualresult == response.error
+
+
+@pytest.mark.parametrize("param,value,code,description",
+                         [
+                             pytest.param("id", 3.14, "DR-2/3",
+                                          "Data type mismatch."
+                                          " Expected data type: 'STRING', actual data type: 'NUMBER'.",
+                                          marks=pytestrail.case('C8354')),
+                             pytest.param("id", True, "DR-2/3",
+                                          "Data type mismatch."
+                                          " Expected data type: 'STRING', actual data type: 'BOOLEAN'.",
+                                          marks=pytestrail.case('C8355')),
+                             pytest.param("id", "", "DR-4/3",
+                                          "Data format mismatch."
+                                          " Expected data format: 'uuid', actual value: ''.",
+                                          marks=pytestrail.case('C8356')),
+                             pytest.param("action", 3.14, "DR-2/3",
+                                          "Data type mismatch."
+                                          " Expected data type: 'STRING', actual data type: 'NUMBER'.",
+                                          marks=pytestrail.case('C8359')),
+                             pytest.param("action", True, "DR-2/3",
+                                          "Data type mismatch."
+                                          " Expected data type: 'STRING', actual data type: 'BOOLEAN'.",
+                                          marks=pytestrail.case('C8360')),
+                             pytest.param("action", "", "DR-3/3",
+                                          "Attribute value mismatch with one of enum expected values."
+                                          f" Expected values: '{', '.join(action_enum)}', actual value: ''.",
+                                          marks=pytestrail.case('C8358')),
+                             pytest.param("action", "checkItems", "DR-3/3",
+                                          "Attribute value mismatch with one of enum expected values."
+                                          f" Expected values: '{', '.join(action_enum)}', actual value: 'checkItems'.",
+                                          marks=pytestrail.case('C8357')),
+                             pytest.param("version", 3.14, "DR-2/3",
+                                          "Data type mismatch."
+                                          " Expected data type: 'STRING', actual data type: 'NUMBER'.",
+                                          marks=pytestrail.case('C8364')),
+                             pytest.param("version", True, "DR-2/3",
+                                          "Data type mismatch."
+                                          " Expected data type: 'STRING', actual data type: 'BOOLEAN'.",
+                                          marks=pytestrail.case('C8363')),
+                             pytest.param("version", "", "DR-4/3",
+                                          "Data format mismatch."
+                                          " Expected data format: '00.00.00', actual value: ''.",
+                                          marks=pytestrail.case('C8365'))
+                         ])
+def test_on_there_is_response_with_status_error_if_request_contains(port, host, param, value, code, description,
+                                                                    response_error,
+                                                                    payload_getLotStateByIds):
+    payload = payload_getLotStateByIds()
+    payload[param] = value
+
+    actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
+
+    expectedresult = response_error
+    expectedresult['result'] = [
+        {
+            "code": code,
+            "description": description,
+            "details": [
+                {
+                    "name": param
+                }
+            ]
+        }
+    ]
+
+    if param == "id":
+        expectedresult['id'] = "00000000-0000-0000-0000-000000000000"
+    if param in {'action', "id"}:
+        expectedresult['version'] = '2.0.0'
+
+        assert actualresult == expectedresult, print(actualresult)
+
+
+@pytest.mark.parametrize("param",
+                         [
+                             pytest.param("version", marks=pytestrail.case('C8361')),
+                             pytest.param("params", marks=pytestrail.case('C8366'))
+
+                         ])
+def test_on_there_is_response_with_status_error_if_request_does_not_contain(port, host, param,
+                                                                            payload_getLotStateByIds,
+                                                                            response_error
+                                                                            ):
+    payload = payload_getLotStateByIds()
+    del payload[param]
+
+    actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
+
+    expectedresult = response_error
+    expectedresult['result'] = [
+        {
+            "code": "DR-1/3",
+            "description": "Missing required attribute.",
+            "details": [
+                {
+                    "name": param
+                }
+            ]
+        }
+    ]
+    if param in {"version"}:
+        expectedresult['version'] = '1.0.0'
+
+    assert actualresult == expectedresult
+
+
+@pytest.mark.parametrize("param,value",
+                         [
+                             pytest.param("params", [{}], marks=pytestrail.case('C8367'))
+                         ])
+def test_on_eAccess_with_inccorect_params_in_payload(host, port, param, value, response_error,
+                                                     payload_getLotStateByIds):
+    payload = payload_getLotStateByIds()
+    payload[param] = value
+    actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
+
+    expectedresult = response_error
+    expectedresult['result'] = [
+        {
+            "code": "RQ-02/3",
+            "description": "Can not parse 'params'."
+        }
+    ]
+
+    assert actualresult == expectedresult, payload
+
+
+@pytest.mark.parametrize("param",
+                         [
+                             pytest.param("version", marks=pytestrail.case('C8410')),
+                             pytest.param("params",
+                                          marks=[pytestrail.case('C8422'),
+                                                 pytest.mark.skip(reason="deprecated")])
+
+                         ])
+def test_eAccess_returns_response_with_status_error_if_request_does_not_contain_param(port, host, param, response,
+                                                                                      payload_checkAccessToTender):
+    payload = payload_checkAccessToTender()
+    del payload[param]
+    actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
+    response.error['result'] = [
+        {
+            "code": "DR-1/3",
+            "description": "Missing required attribute.",
+            "details": [{"name": param}]
+        }
+    ]
+    if param == "version":
+        response.error['version'] = '1.0.0'
+
+    assert actualresult == response.error
+
+
+@pytest.mark.parametrize("param,value",
+                         [
+                             pytest.param("params", [{}], marks=pytestrail.case('C8425'))
+                         ])
+def test_on_dataValidation_with_inccorect_params_in_payload(host, port, param, value, response,
+                                                            payload_checkAccessToTender):
+    payload = payload_checkAccessToTender()
+    payload[param] = value
+    actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
+    response.error['result'] = [
+        {
+            "code": "RQ-02/3",
+            "description": "Can not parse 'params'."
+        }
+    ]
+
+    assert actualresult == response.error
