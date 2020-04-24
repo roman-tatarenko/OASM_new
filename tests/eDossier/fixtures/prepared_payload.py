64c0a728f0942bd2d9bbac3abc7f9466b2041c31
Roman <tatarenko.roman@gmail.com>
2020-04-24 10:40:05 +0300

    eDossier/fixtutes/prepared_payload refactor:: payload_validateRequirementResponse:: changed prepared_entity_id -> uuid4

--- tests/eDossier/fixtures/prepared_payload.py
+++ tests/eDossier/fixtures/prepared_payload.py
@@ -1,15 +1,14 @@
 import pytest
+from uuid import uuid4
 
 
 @pytest.fixture(scope='function')
-def payload_validateRequirementResponse(request_template, prepared_cpid, prepared_ev_ocid, prepared_entity_id):
+def payload_validateRequirementResponse(request_template, prepared_cpid, prepared_entity_id):
     payload = request_template(action='validateRequirementResponse')
 
-    def _payload_validateRequirementResponse(requirement_id=str(prepared_entity_id()), cpid=prepared_cpid,
-                                             ocid=prepared_ev_ocid, value=True):
+    def _payload_validateRequirementResponse(requirement_id=str(uuid4()), cpid=prepared_cpid, value=True):
         payload['params'] = {
             "cpid": cpid,
-            "ocid": ocid,
             "requirementResponse": {
                 "id": "string",
                 "value": value,
