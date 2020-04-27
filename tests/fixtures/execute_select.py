f6d88096fa781d3d2fc803c1c37cbd5862c9535d
unwnu <ruslansolominskii@gmail.com>
2020-04-27 21:15:14 +0300

    fixtures/execute_select::add::execute_select_evaluation_award_by_token_entity

--- tests/fixtures/execute_select.py
+++ tests/fixtures/execute_select.py
@@ -15,3 +15,12 @@ def execute_select_notice_compiled_release(prepared_select_notice_compiled_relea
         return cassandra_session.execute(prepared_select_notice_compiled_release, [cpid, ocid])
 
     return with_values
+
+
+@pytest.fixture(scope='function')
+def execute_select_evaluation_award_by_token_entity(prepared_select_evaluation_award_by_token_entity,
+                                                    cassandra_session):
+    def with_values(cp_id, stage, token_entity):
+        return cassandra_session.execute(prepared_select_evaluation_award_by_token_entity, [cp_id, stage, token_entity])
+
+    return with_values
