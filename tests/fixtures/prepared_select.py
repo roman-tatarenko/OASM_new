59550a0f2e814fa174514c1b28f23b8809b1d71c
unwnu <ruslansolominskii@gmail.com>
2020-04-27 21:11:45 +0300

    fixtures/prepared_select add::prepared_select_evaluation_award_by_token_entity

--- tests/fixtures/prepared_select.py
+++ tests/fixtures/prepared_select.py
@@ -15,3 +15,11 @@ def prepared_select_notice_compiled_release(cassandra_session):
     query = 'SELECT * FROM notice_compiled_release WHERE cp_id=? AND oc_id=?'
     prepared = cassandra_session.prepare(query)
     return prepared
+
+
+@pytest.fixture(scope='session')
+def prepared_select_evaluation_award_by_token_entity(cassandra_session):
+    cassandra_session.set_keyspace('ocds')
+    query = 'SELECT * FROM evaluation_award WHERE cp_id=? AND stage=? AND token_entity=?'
+    prepared = cassandra_session.prepare(query)
+    return prepared
