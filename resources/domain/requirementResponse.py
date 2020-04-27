52f32ef13843bd1e91e36675382e952600447c00
unwnu <ruslansolominskii@gmail.com>
2020-04-27 21:16:24 +0300

    domain/requirementResponse add::requirementResponse

---
+++ resources/domain/requirementResponse.py
@@ -0,0 +1,17 @@
+schema_requirementResponse = {
+    "id": "2f3b8e5d-42ce-47f7-a35c-ec8ec66bba73",
+    "value": "true",
+    "relatedTenderer": {
+        "id": "569fe9f8-f941-4d70-a9cd-033c39c6b47b"
+    },
+    "requirement": {
+        "id": "965925c2-46bc-4247-8673-7951755d70ef"
+    },
+    "responder": {
+        "identifier": {
+            "scheme": "",
+            "id": "string"
+        },
+        "name": "responderName"
+    }
+}
