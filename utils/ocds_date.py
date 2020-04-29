2ef3927d9adda6e905fb516fc2be06ced3342e63
unwnu <ruslansolominskii@gmail.com>
2020-04-29 21:37:38 +0300

    utils/ocds_date add::ocds_datetime, ocds_date_to_datetime

---
+++ utils/ocds_date.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+
+def ocds_datetime() -> str:
+    """
+    OCDS makes use of ISO8601 date-times.
+    """
+    return (datetime.utcnow()).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+
+def ocds_date_to_datetime(str_datetime: str):
+    return datetime.strptime(str_datetime, "%Y-%m-%dT%H:%M:%SZ")
