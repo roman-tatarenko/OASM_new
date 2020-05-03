dfe63072e87067727a76cd45316f93ebd44d2569
unwnu <ruslansolominskii@gmail.com>
2020-05-03 21:09:53 +0300

    conftest.py chore::del comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -45,12 +45,6 @@ def port():
 
 @pytest.fixture(scope='session')
 def md5():
-    # def _md5(file_name):
-    #     hash_md5 = hashlib.md5()
-    #     with open(file_name, "rb") as f:
-    #         for chunk in iter(lambda: f.read(4096), b""):
-    #             hash_md5.update(chunk)
-    #     return hash_md5.hexdigest()
     def _md5(file_path):
         return hashlib.md5(open(file_path, 'rb').read()).hexdigest()
 
