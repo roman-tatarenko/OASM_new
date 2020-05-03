c20a607ac4552c079eb72b4dea320188c830aea5
unwnu <ruslansolominskii@gmail.com>
2020-05-03 21:09:17 +0300

    conftest.py refactor::merging comparisons with "in" to "cli_value in ('', 'dev')"

--- conftest.py
+++ conftest.py
@@ -1,25 +1,26 @@
 import pytest
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.cluster import Cluster
 
 from credentials import hosts, cacluster, bootstrap_servers
 
 
 def pytest_addoption(parser):
     """Method for adding custom console parameters"""
-    parser.addoption('--host',
-                     default='dev',
-                     help='host options: "dev", "sandbox", or your own host for local testing')
+    parser.addoption(
+        '--host',
+        default='dev',
+        help='host options: "dev", "sandbox",'
+             ' or your own host for local testing'
+    )
 
 
 @pytest.fixture(scope='session')
 def host(request):
-    """Return the target host
-    :param request:
-    :return:
-    """
-    # get host value
+    """Return the target host"""
     cli_value = request.config.getoption('--host')
 
-    if cli_value == '' or cli_value == 'dev':
+    if cli_value in {'', 'dev'}:
         domain = hosts['dev']
     elif cli_value == 'sandbox':
         domain = hosts['sandbox']
@@ -30,21 +31,29 @@ def host(request):
 
 @pytest.fixture(scope='session')
 def cluster(request):
-    from cassandra.auth import PlainTextAuthProvider
-    from cassandra.cluster import Cluster
-
-    # get host value
     cli_value = request.config.getoption('--host')
 
-    if cli_value == '' or cli_value == 'dev':
-        auth_provider = PlainTextAuthProvider(username=cacluster['dev']['username'],
-                                              password=cacluster['dev']['password'])
-        cluster = Cluster(cacluster['dev']['cluster'], auth_provider=auth_provider, port=9042)
+    if cli_value in {'', 'dev'}:
+        auth_provider = PlainTextAuthProvider(
+            username=cacluster['dev']['username'],
+            password=cacluster['dev']['password']
+        )
+        cluster = Cluster(
+            cacluster['dev']['cluster'],
+            auth_provider=auth_provider,
+            port=9042
+        )
 
     elif cli_value == 'sandbox':
-        auth_provider = PlainTextAuthProvider(username=cacluster['sandbox']['username'],
-                                              password=cacluster['sandbox']['password'])
-        cluster = Cluster(cacluster['sandbox']['cluster'], auth_provider=auth_provider, port=9042)
+        auth_provider = PlainTextAuthProvider(
+            username=cacluster['sandbox']['username'],
+            password=cacluster['sandbox']['password']
+        )
+        cluster = Cluster(
+            cacluster['sandbox']['cluster'],
+            auth_provider=auth_provider,
+            port=9042
+        )
 
     return cluster
 
