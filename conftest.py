87dbc56d10225054b79cf3e897192c45903e089c
Ruslan Solominskii <52795077+unwnu@users.noreply.github.com>
2020-05-07 13:47:56 +0300

    feat(project configuration): configuration approach change
    - add integration with tox(tox.ini);
    - add config.toml in project;
    - change name addoption --host -> --env(conftest.py);
    - add package: pytest_check, toml, pytest-sugar,pytest-testrail-e2e
    - change package pytest-testrail -> pytest-testrail-e2e;
    - remove credentials.py from project;

--- conftest.py
+++ conftest.py
@@ -1,29 +1,30 @@
 import pytest
+import toml
 from cassandra.auth import PlainTextAuthProvider
 from cassandra.cluster import Cluster
 
-from credentials import hosts, cacluster, bootstrap_servers
+config = toml.load('config.toml')
 
 
 def pytest_addoption(parser):
     """Method for adding custom console parameters"""
     parser.addoption(
-        '--host',
+        '--env',
         default='dev',
-        help='host options: "dev", "sandbox",'
-             ' or your own host for local testing'
+        help='environment options: "dev", "sandbox",'
+             ' or your own host for testing'
     )
 
 
 @pytest.fixture(scope='session')
 def host(request):
     """Return the target host"""
-    cli_value = request.config.getoption('--host')
+    cli_value = request.config.getoption('--env')
 
-    if cli_value in {'', 'dev'}:
-        domain = hosts['dev']
+    if cli_value == 'dev':
+        domain = config['development']['host']
     elif cli_value == 'sandbox':
-        domain = hosts['sandbox']
+        domain = config['sandbox']['host']
     else:
         domain = cli_value
     return domain
@@ -31,26 +32,26 @@ def host(request):
 
 @pytest.fixture(scope='session')
 def cluster(request):
-    cli_value = request.config.getoption('--host')
+    cli_value = request.config.getoption('--env')
 
-    if cli_value in {'', 'dev'}:
+    if cli_value == 'dev':
         auth_provider = PlainTextAuthProvider(
-            username=cacluster['dev']['username'],
-            password=cacluster['dev']['password']
+            username=config['development']['cassandra']['username'],
+            password=config['development']['cassandra']['password']
         )
         cluster = Cluster(
-            cacluster['dev']['cluster'],
+            config['development']['cassandra']['cluster'],
             auth_provider=auth_provider,
             port=9042
         )
 
     elif cli_value == 'sandbox':
         auth_provider = PlainTextAuthProvider(
-            username=cacluster['sandbox']['username'],
-            password=cacluster['sandbox']['password']
+            username=config['sandbox']['cassandra']['username'],
+            password=config['sandbox']['cassandra']['password']
         )
         cluster = Cluster(
-            cacluster['sandbox']['cluster'],
+            config['sandbox']['cassandra']['cluster'],
             auth_provider=auth_provider,
             port=9042
         )
@@ -60,13 +61,13 @@ def cluster(request):
 
 @pytest.fixture(scope='session')
 def kafka_bootstrap_servers(request):
-    cli_value = request.config.getoption('--host')
+    cli_value = request.config.getoption('--env')
     if cli_value == '' or cli_value == 'dev':
-        bs = bootstrap_servers['dev']
+        bs = config['development']['botstrap_servers']
     return bs
 
 
-@pytest.fixture(scope='session')
+@pytest.yield_fixture(scope='session')
 def cassandra_session(cluster):
     session = cluster.connect()
     yield session
