6966a10547adc6fa46fad2fc05699137fdb05964
tatarenko245 <tatarenko.roman@gmail.com>
2020-07-10 11:00:20 +0300

    refacor(tests/eAccess/fixtures/prepared_payload) del payload for eQualification:checkAccessToQualification

--- tests/eAccess/fixtures/prepared_payload.py
+++ tests/eAccess/fixtures/prepared_payload.py
@@ -129,20 +129,3 @@ def payload_setStateForLots(request_template, prepared_cpid, prepared_ev_ocid):
     return _payload_setStateForLots
 
 
-@pytest.fixture(scope='function')
-def payload_checkAccessToQualification(request_template, prepared_cpid, prepared_tp_ocid, prepared_owner,
-                                       prepared_token_entity, prepared_entity_id):
-    payload = request_template(action='checkAccessToQualification')
-
-    def _payload_checkAccessToQualification(cpid=prepared_cpid, ocid=prepared_tp_ocid, token=str(prepared_token_entity),
-                                            owner=prepared_owner, qualificationId=str(uuid4())):
-        payload['params'] = {
-            "cpid": cpid,
-            "ocid": ocid,
-            "token": token,
-            "owner": owner,
-            "qualificationId": qualificationId
-        }
-        return payload
-
-    return _payload_checkAccessToQualification
