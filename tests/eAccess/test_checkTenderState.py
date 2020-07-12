bb52391e814d3b8d916b99c8dd971b301492a22b
roman <tatarenko.roman@gmail.com>
2020-07-12 23:07:23 +0300

    feature(tests/eAccess/test_checkTenderState

--- tests/eAccess/test_checkTenderState.py
+++ tests/eAccess/test_checkTenderState.py
@@ -254,40 +254,128 @@ def test_checkTenderState_check_on_service_return_error_in_response(execute_inse
     print(json.dumps(actualresult))
     assert actualresult == response.error
 
-# @pytestrail.case('C23074')
-# def test_checkTenderState_check_on_service_return error in response if use invalid operationType(execute_insert_into_access_tender, prepared_cpid,
-#                                                       prepare_data, prepared_token_entity,
-#                                                       prepared_owner,
-#                                                       payload_checkTenderState, host, port, response,
-#                                                       prepared_tp_ocid):
-#     cpid = prepared_cpid
-#     data = prepare_data(schema=schema_tender_GPA, quantity=1)
-#     data['ocid'] = cpid
-#     data['tender']['statusDetails'] = "empty"
-#
-#     execute_insert_into_access_tender(
-#         cp_id=cpid,
-#         stage="TP",
-#         token_entity=prepared_token_entity,
-#         created_date=datetime.now(),
-#         json_data=data,
-#         owner=prepared_owner
-#     )
-#
-#     payload = payload_checkTenderState(
-#         cpid=cpid,
-#         ocid=prepared_tp_ocid,
-#         operationType="qualificationConsideration"
-#     )
-#     actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
-#
-#     response.error['result'] = [{
-#         "code": "VR.COM-1.17.2/3",
-#         "description": f"Tender with id='{data['tender']['id']}' has invalid states.",
-#         "details": [{
-#             "id": f"{data['tender']['id']}"
-#         }]
-#     }]
-#     print(json.dumps(data))
-#     print(json.dumps(actualresult))
-#     assert actualresult == response.error
+
+@pytestrail.case('C23074')
+def test_checkTenderState_error_in_response_if_use_invalid_operationType(execute_insert_into_access_tender,
+                                                                         prepared_cpid,
+                                                                         prepare_data, prepared_token_entity,
+                                                                         prepared_owner,
+                                                                         payload_checkTenderState, host, port, response,
+                                                                         prepared_tp_ocid):
+    cpid = prepared_cpid
+    data = prepare_data(schema=schema_tender_GPA, quantity=1)
+    data['ocid'] = cpid
+    data['tender']['status'] = "active"
+    data['tender']['statusDetails'] = "qualification"
+
+    execute_insert_into_access_tender(
+        cp_id=cpid,
+        stage="TP",
+        token_entity=prepared_token_entity,
+        created_date=datetime.now(),
+        json_data=data,
+        owner=prepared_owner
+    )
+
+    payload = payload_checkTenderState(
+        cpid=cpid,
+        ocid=prepared_tp_ocid,
+        operationType="quali"
+    )
+    actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
+
+    response.error['result'] = [{
+        "code": "DR-3/3",
+        "description": f"Attribute value mismatch with one of enum expected values. "
+                       f"Expected values: 'qualification, qualificationConsideration', actual value: 'quali'.",
+        "details": [{
+            "name": "operationType"
+        }]
+    }]
+    print(json.dumps(data))
+    print(json.dumps(actualresult))
+    assert actualresult == response.error
+
+
+@pytest.mark.parametrize("param",
+                         [
+                             pytest.param("cpid",
+                                          marks=pytestrail.case('C23090'),
+                                          id="del cpid"),
+                             pytest.param("ocid",
+                                          marks=pytestrail.case('C23091'),
+                                          id="del ocid"),
+                             pytest.param("pmd",
+                                          marks=pytestrail.case('C23092'),
+                                          id="del pmd"),
+                             pytest.param("country",
+                                          marks=pytestrail.case('C23093'),
+                                          id="del country"),
+                             pytest.param("operationType",
+                                          marks=pytestrail.case('C23094'),
+                                          id="del operationId")
+                         ])
+def test_checTenderState_without_attribute_in_params_object(prepared_cpid, payload_checkTenderState, host, port,
+                                                            response,
+                                                            prepared_tp_ocid, param):
+    payload = payload_checkTenderState(
+        cpid=prepared_cpid,
+        ocid=prepared_tp_ocid,
+        operationType="qualificationConsideration"
+    )
+
+    del payload['params'][param]
+    actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
+
+    response.error['result'] = [{
+        "code": "RQ-02/3",
+        "description": "Can not parse 'params'."
+    }]
+
+    assert actualresult == response.error
+
+
+@pytest.mark.parametrize("param, value,code, description",
+                         [
+                             pytest.param("cpid", False, "DR-5/3", f"Data mismatch to pattern: "
+                                                                   f"'^[a-z]{4}-[a-z0-9]{6}-[A-Z]{2}-[0-9]{13}$'. "
+                                                                   f"Actual value: 'false'.",
+                                          marks=pytestrail.case('C23095'),
+                                          id="cpid ==False"),
+                             pytest.param("ocid", False, "DR-5/3", f"Data mismatch to pattern: "
+                                                                   f"'^[a-z]{4}-[a-z0-9]{6}-[A-Z]{2}-[0-9]{13}-"
+                                                                   f"(AC|EI|EV|FS|NP|PN|TP)-[0-9]{13}$'. "
+                                                                   f"Actual value: 'false'.",
+                                          marks=pytestrail.case('C23096'),
+                                          id="ocid == False"),
+                             pytest.param("pmd", 123, "DR-3/3", f"Attribute value mismatch with one of enum expected "
+                                                                f"values. Expected values: 'MV, OT, RT, SV, DA, NP, FA,"
+                                                                f" OP, GPA, TEST_OT, TEST_SV, TEST_RT, TEST_MV, TEST_DA,"
+                                                                f" TEST_NP, TEST_FA, TEST_OP, TEST_GPA', actual value:"
+                                                                f" '123'.",
+                                          marks=pytestrail.case('C23097'),
+                                          id="del pmd == 123")
+
+                         ])
+def test_checTenderState_use_invalid_value_in_prarams(prepared_cpid, payload_checkTenderState, host, port,
+                                                      response, value, code, description,
+                                                      prepared_tp_ocid, param):
+    payload = payload_checkTenderState(
+        cpid=prepared_cpid,
+        ocid=prepared_tp_ocid,
+        operationType="qualificationConsideration"
+    )
+
+    payload['params'][param] = value
+    actualresult = requests.post(f'{host}:{port.eAccess}/command2', json=payload).json()
+
+    response.error['result'] = [{
+        "code": code,
+        "description": description,
+        "details": [{
+            "name": param
+        }]
+
+    }]
+
+    assert actualresult == response.error
